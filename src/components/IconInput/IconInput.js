fa7a35c320827ec544b2f5975230900aaad8852c
Xuan Bui <xbui@overdrive.com>
2022-03-25 14:31:33 -0700

    Implement icon input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,25 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const IconInputStyles = {
+  small: {
+    strokeWidth: 1,
+    '--icon-size': 16,
+    '--border-width': 1 + 'px',
+    '--font-size': 14 / 16 + 'rem',
+    '--padding': 4 + 'px',
+    '--padding-left': 8 + 16 + 'px'
+  },
+  large: {
+    strokeWidth: 2,
+    '--icon-size': 24,
+    '--border-width': 2 + 'px',
+    '--font-size': 18 / 16 + 'rem',
+    '--padding': 7 + 'px',
+    '--padding-left': 12 + 24 + 'px'
+  }
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +32,58 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const styles = IconInputStyles[size];
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{ ...styles }}>
+        <Icon id={icon} size={styles['--icon-size']} strokeWidth={styles.strokeWidth}></Icon>
+      </IconWrapper>
+      <Input width={width} placeholder={placeholder} style={{ ...styles }}></Input>
+    </Wrapper>
+  );
 };
 
 export default IconInput;
+
+const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const Input = styled.input`
+  width: ${props => props.width + 'px'};
+  padding: var(--padding);
+  padding-left: var(--padding-left);
+  appearance: none;
+  border: none;
+  border-bottom: solid black;
+  border-bottom-width: var(--border-width);
+  font-size: var(--font-size);
+  outline-offset: 2px;
+  color: inherit;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+  }
+
+  &:not(:placeholder-shown) {
+    font-weight: 700;
+  }
+`;
+
+const IconWrapper = styled.div`
+  width: fit-content;
+  height: fit-content;
+  margin: auto;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: var(--padding);
+  pointer-events: none;
+`;
