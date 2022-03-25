2d9e76cd0e05db8ec54a70420ed6242b7f22affa
Xuan Bui <xbui@overdrive.com>
2022-03-25 12:11:03 -0700

    Implement select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,45 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper value={value} onChange={onChange}>
+      <SelectElement value={value} onChange={onChange}>{children}</SelectElement>
+      <IconWrapper>
+        <Icon id='chevron-down' size={24} strokeWidth={2}></Icon>
+      </IconWrapper>
+    </Wrapper>
   );
 };
 
 export default Select;
+
+const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const SelectElement = styled.select`
+  padding: 12px;
+  padding-right: 52px;
+  font-size: 1rem;
+  color: inherit;
+  background-color: ${COLORS.transparentGray15};
+  border: none;
+  border-radius: 8px;
+  appearance: none;
+`;
+
+const IconWrapper = styled.div`
+  width: fit-content;
+  height: fit-content;
+  margin: auto;
+  position: absolute;
+  top: 0;
+  right: 12px;
+  bottom: 0;
+  pointer-events: none;
+`;
